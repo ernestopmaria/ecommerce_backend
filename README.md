f5a56e7e7d1fef0af82580f6dc7591e524416396
ernesto.maria <ernestomaria93@gmail.com>
2022-11-30 23:15:33 +0100

    redis adding

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ecommerce_backend
+
+# Docker comand for redis client
+
+docker run -v redisinsight:/db -p 8001:8001 redislabs/redisinsight:latest
+
+# redis cli
+
+docker exec -it redis sh
+redis cli
--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,11 +1,21 @@
 import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Products';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
 	public async execute(): Promise<Product[]> {
 		const productRepository = getCustomRepository(ProductRepository);
-		const products = await productRepository.find();
+
+		const redisCache = new RedisCache();
+		let products = await redisCache.recover<Product[]>(
+			'api_vendas-PRODUCT_LIST',
+		);
+		if (!products) {
+			products = await productRepository.find();
+			await redisCache.save('api_vendas-PRODUCT_LIST', products);
+		}
+
 		return products;
 	}
 }
