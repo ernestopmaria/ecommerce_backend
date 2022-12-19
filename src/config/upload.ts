e273b4fd545a9e3daf280669348c5ec562afbb83
ernesto.maria <ernestomaria93@gmail.com>
2022-12-19 10:50:18 +0100

    amazon s3 config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,42 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+interface IUploadConfig {
+	driver: 's3' | 'disk';
+	tmpFolder: string;
+	directory: string;
+	multer: {
+		storage: StorageEngine;
+	};
+	config: {
+		aws: {
+			bucket: string;
+		};
+	};
+}
+
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
-const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
 	directory: uploadFolder,
 	tmpFolder,
-	storage: multer.diskStorage({
-		destination: uploadFolder,
-		filename(request, file, callback) {
-			const fileHash = crypto.randomBytes(10).toString('hex');
+	multer: {
+		storage: multer.diskStorage({
+			destination: tmpFolder,
+			filename(request, file, callback) {
+				const fileHash = crypto.randomBytes(10).toString('hex');
 
-			const filename = `${fileHash}-${file.originalname}`;
+				const filename = `${fileHash}-${file.originalname}`;
 
-			callback(null, filename);
+				callback(null, filename);
+			},
+		}),
+	},
+	config: {
+		aws: {
+			bucket: 'api-minhapraca',
 		},
-	}),
-};
+	},
+} as IUploadConfig;
--- src/modules/users/services/UpdateUserAvatarServices.ts
+++ src/modules/users/services/UpdateUserAvatarServices.ts
@@ -1,10 +1,10 @@
 import AppError from '@shared/errors/AppError';
-import path from 'path';
-import fs from 'fs';
+
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UserRepository from '../typeorm/repositories/UserRepository';
-import uploadConfig from '@config/upload';
+
+import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
 
 interface IRequest {
 	user_id: string;
@@ -14,6 +14,7 @@ interface IRequest {
 class UpdateUserAvatarServices {
 	public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
 		const userRepository = getCustomRepository(UserRepository);
+		const storageProvider = new DiskStorageProvider();
 
 		const user = await userRepository.findById(user_id);
 
@@ -22,13 +23,11 @@ class UpdateUserAvatarServices {
 		}
 
 		if (user.avatar) {
-			const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-			const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-			if (userAvatarFileExists) {
-				await fs.promises.unlink(userAvatarFilePath);
-			}
+			await storageProvider.deleteFile(user.avatar);
 		}
-		user.avatar = avatarFilename;
+		const filename = await storageProvider.saveFile(avatarFilename);
+
+		user.avatar = filename;
 
 		await userRepository.save(user);
 
